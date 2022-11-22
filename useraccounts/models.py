e99a6896a7de8a00271cc5ee5a2d535b8f2f5986
sireesha <msireesha30031999@gmail.com>
2022-11-22 15:30:17 +0530

    addded profile model

--- useraccounts/models.py
+++ useraccounts/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+
+GENDER_CHOICES = (
+    ("M", "Male"),
+    ("F", "Female"),
+    ("O", "OTHER"),
+)
 
 
 class Profile(models.Model):
     """model for user account"""
-    image = models.ImageField(upload_to="profileimages")
-    sex = models.CharField(max_length=110)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profileimages/', default='profileimages/profile-avatar.png', null=False,
+                              blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False)
+    username = models.CharField(max_length=150, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False, unique=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
     def __str__(self):
-        return self.user.username
+        return self.username
