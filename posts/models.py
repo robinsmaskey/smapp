219df59c48dad3d0865e2d8507d7ef2725cd8664
sireesha <msireesha30031999@gmail.com>
2022-11-25 13:07:09 +0530

    Post and Comments models created and added to admin site

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import uuid
+from useraccounts.models import Profile
 
-# Create your models here.
+
+class Post(models.Model):
+    """Post Model"""
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=False, blank=False, related_name='posts')
+    title = models.CharField(max_length=50, null=False, blank=False)
+    content = models.CharField(max_length=200, null=False, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True, blank=False, null=False)
+    likes = models.ManyToManyField(Profile, blank=True, related_name='postlikes')
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """Comment Model"""
+    content = models.CharField(max_length=200, null=False, blank=False)
+    owner = models.ForeignKey(Profile, on_delete=models.PROTECT, null=False, blank=False, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=False, blank=False, related_name='comments')
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+
+    def __str__(self):
+        return str(self.post) + " " + str(self.owner)
+
+
+
