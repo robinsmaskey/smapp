0aa12dbd43b8b2a848f26ea295145ba78663d3af
sireesha <msireesha30031999@gmail.com>
2022-12-03 11:53:45 +0530

    static url added

--- SocialMedia_App_Django_Backend/urls.py
+++ SocialMedia_App_Django_Backend/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('posts/', include('posts.urls')),
 
 ]
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
